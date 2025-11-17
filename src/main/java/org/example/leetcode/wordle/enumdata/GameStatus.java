3cbbe5ae90d02f4c909d13dc852468ba5e182c9d
WenJanzeMac <zxc80376@gmail.com>
2025-11-17 22:53:45 +0800

    Add GameStatus Overed

--- src/main/java/org/example/leetcode/wordle/enumdata/GameStatus.java
+++ src/main/java/org/example/leetcode/wordle/enumdata/GameStatus.java
@@ -2,9 +2,10 @@ package org.example.leetcode.wordle.enumdata;
 
 
 public enum GameStatus {
-    WARG(0, "錯誤"),
+    WARING(0, "錯誤"),
     WIN(1, "您贏了"),
-    Fail(2, "您失敗了");;
+    Fail(2, "您失敗了"),
+    OVERED(3,"已結束" );;
     private final int code;
     private final String desc;
 
