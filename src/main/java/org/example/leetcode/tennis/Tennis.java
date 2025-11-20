942d83503e2b7c15dcba49a2d16dc9b54a160f10
Wilson Haung <wilson@cloud-latitude.com>
2025-11-20 18:00:42 +0800

    refactor method let code easy read

--- src/main/java/org/example/leetcode/tennis/Tennis.java
+++ src/main/java/org/example/leetcode/tennis/Tennis.java
@@ -27,26 +27,35 @@ public class Tennis {
 
         String aScoreString = getScoreStringText(teamAScoreCount);
         String bScoreString = getScoreStringText(teamBScoreCount);
-
         boolean gamePoint = teamAScoreCount > 3 || teamBScoreCount > 3;
         boolean isSameScore = teamAScoreCount == teamBScoreCount;
         boolean deuce = teamAScoreCount >= 3 && isSameScore;
 
-        if (deuce) {
-            result.append("deuce");
-        } else if (Math.abs(teamAScoreCount - teamBScoreCount) > 1 && gamePoint) {
-            result.append( getAdvantageTeam() )
-                    .append(" win");
-        } else if (teamAScoreCount >= 3 && teamBScoreCount >= 3) {
-            result.append( getAdvantageTeam() )
-                    .append(" adv");
-        } else {
-            result.append(aScoreString)
+        GameStatus gameStatus = getGameStatus(deuce, gamePoint);
+        switch (gameStatus) {
+            case DEUCE -> result.append("deuce");
+            case WIN -> result.append(getAdvantageTeam()).append(" win");
+            case ADVANTAGE -> result.append(getAdvantageTeam()).append(" adv");
+            case CONDUCT -> result.append(aScoreString)
                     .append("-")
                     .append(isSameScore ? "all" : bScoreString);
+
         }
         return result.toString();
     }
+
+    private GameStatus getGameStatus(boolean deuce, boolean gamePoint) {
+        if (deuce) {
+            return GameStatus.DEUCE;
+        } else if (Math.abs(teamAScoreCount - teamBScoreCount) > 1 && gamePoint) {
+            return GameStatus.WIN;
+        } else if (teamAScoreCount >= 3 && teamBScoreCount >= 3) {
+            return GameStatus.ADVANTAGE;
+        } else {
+            return GameStatus.CONDUCT;
+        }
+    }
+
 
     private String getAdvantageTeam() {
         return teamAScoreCount > teamBScoreCount ? "a" : "b";
@@ -57,5 +66,12 @@ public class Tennis {
             return scoreList.get(3);
         }
         return scoreList.get(count);
+    }
+
+    enum GameStatus {
+        DEUCE,
+        WIN,
+        ADVANTAGE,
+        CONDUCT
     }
 }
